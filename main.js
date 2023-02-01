32d974462fdff5679164eb2a6e50fbf2f0e64e6c
Benjamin <bnjackson951@gmail.com>
2023-02-01 22:01:28 +0000

    Added function displayTime. Small changes to displayWelcomeMessage

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-const d = new Date();
-const hour = d.getHours();
+let d = new Date();
+let hours = d.getHours();
 
-const setWelcomeMessage = () => {
-    const welcomeMessage = document.getElementById('welcome-message');
-    if(hour >= 5 && hour < 12) {
-        welcomeMessage.innerHTML = 'Good Morning';
-    } else if (hour >= 12 && hour < 18) {
-        welcomeMessage.innerHTML = 'Good Afternoon';
-    } else if (hour >= 18 && hour < 24) {
-        welcomeMessage.innerHTML = 'Good Evening';
-    } else if (hour >= 24 && hour < 5) {
-        welcomeMessage.innerHTML = 'Good Night';
-    }    
+const displayTime = () => {
+    d = new Date();
+    hours = d.getHours();
+    let minutes = d.getMinutes();
+    let currentTime = `${hours < 10 ? "0" + hours : hours} : ${minutes < 10 ? "0" + minutes : minutes} - `;
+    let time = document.getElementById("time");
+    time.innerText = currentTime;
+    setTimeout(displayTime, 1000);
+    console.log(currentTime, minutes);
 }
+
+
+
+const displayWelcomeMessage = () => {
+    d = new Date();
+    hours = d.getHours();
+    const welcomeMessage = document.getElementById('welcome-message');
+    if(hours >= 5 && hours < 12) {
+        welcomeMessage.innerText = 'Good morning';
+    } else if (hours >= 12 && hours < 18) {
+        welcomeMessage.innerText = 'Good afternoon';
+    } else if (hours >= 18 && hours < 24) {
+        welcomeMessage.innerText = 'Good evening';
+    } else if (hours >= 24 && hours < 5) {
+        welcomeMessage.innerText = 'Good Night';
+    }
+    setTimeout(displayWelcomeMessage, (60 * 1000) * 60);
+}
+
+displayWelcomeMessage();
+displayTime();
